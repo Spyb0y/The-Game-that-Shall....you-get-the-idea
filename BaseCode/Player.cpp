c9e511cef215310ab248971df8ae847d6d55e761
Spyb0y <IamAspyboy@gmail.com>
2014-09-19 16:34:53 -0400

    Player 1.0

--- BaseCode/Player.cpp
+++ BaseCode/Player.cpp
@@ -1,2 +1,69 @@
 #include "Player.h"
+#include "Equipment.h"
 
+
+
+Player::~Player(void)
+{
+}
+
+Player::Player(unsigned Speed, int Health, unsigned Damage, unsigned Evade, unsigned Defense, const unsigned MaxAccessorySlots, const unsigned MaxArmourSlots, unsigned MaxWeaponSlots)
+{
+	
+}
+
+void Player::GetEquipment(Equipment* newItem)
+{
+	if (newItem->isArmor)
+	{
+		armourInventory.push_back(newItem);
+		AddPlayerStats(newItem);
+		if (armourInventory.size >= maxArmourSlots)
+		{
+			//ask player to remove an item
+			DeletePlayerStats(/*removeditem*/);
+		}
+	}
+	if (newItem->isWeapon)
+	{
+		weaponInventory.push_back(newItem);
+		AddPlayerStats(newItem);
+		if (armourInventory.size >= maxAccessorySlots)
+		{
+			//ask player to remove an item
+			DeletePlayerStats(/*removeditem*/);
+		}
+	}
+	if (newItem->isAccessory)
+	{
+		accessoryInventory.push_back(newItem);
+		AddPlayerStats(newItem);
+		if (armourInventory.size >= maxAccessorySlots)
+		{
+			//ask player to remove an item
+			DeletePlayerStats(/*removeditem*/);
+		}
+	}
+}
+
+void Player::AddPlayerStats(Equipment* newItem)
+{
+	//need player
+	mPlayer.Speed = mPlayer.Speed + newItem->Speed;
+	mPlayer.Health = mPlayer.Health + newItem->HP;
+	mPlayer.Damage = mPlayer.Damage + newItem->Attack;
+	mPlayer.Evade = mPlayer.Evade + newItem->Evasion;
+	mPlayer.Defense = mPlayer.Defense + newItem->Defence;
+}
+
+void Player::DeletePlayerStats(Equipment* newItem)
+{
+	//need player
+	mPlayer.Speed = mPlayer.Speed - newItem->Speed;
+	mPlayer.Health = mPlayer.Health - newItem->HP;
+	mPlayer.Damage = mPlayer.Damage - newItem->Attack;
+	mPlayer.Evade = mPlayer.Evade - newItem->Evasion;
+	mPlayer.Defense = mPlayer.Defense - newItem->Defence;
+}
+//when item is gained
+//GetEpuipment();
--- BaseCode/Player.h
+++ BaseCode/Player.h
@@ -1,20 +1,36 @@
 #pragma once
-#include "sprite.h"
-class Player :
-	public Sprite
+#include <vector>
+
+class Player
 {
 private:
+	unsigned speed = 1;
+	int health = 10;
+	unsigned damage = 2;
+	unsigned evade = 5;
+	unsigned defense = 0;
+	unsigned maxAccessorySlots = 1;
+	unsigned maxArmourSlots = 2;
+	unsigned maxWeaponSlots = 2;
+
+
+	void GetEquipment(Equipment* newItem);
+	void AddPlayerStats(Equipment* newItem);
+	void DeletePlayerStats(Equipment* newItem);
 	
 public:
+	std::vector<Equipment*> armourInventory;
+	std::vector<Equipment*> weaponInventory;
+	std::vector<Equipment*> accessoryInventory;
 
-	Player(FXMVECTOR pos, FXMVECTOR scale, uint16_t frameWidth, uint16_t frameHeight, float depth, const std::vector<Frame*>& frames, float frameRate, ID3D11Device* device)
-		:Sprite(pos, scale, frameWidth, frameHeight, depth, frames, frameRate, device)
+	Player(void)
+	{}
+
+	Player(unsigned Speed, int Health, unsigned Damage, unsigned Evade, unsigned Defense, const unsigned MaxAccessorySlots, const unsigned MaxArmourSlots, unsigned MaxWeaponSlots)
 	{
 	}
 
-	//~Player(void);
-
-//	void Jump();
+	~Player(void);
 
 };
 
