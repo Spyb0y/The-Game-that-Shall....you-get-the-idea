7f4ef28e864c0372f4e89796c3810a1b450b3cf0
DanMatrixN7 <danmatrix@live.com>
2014-09-23 16:59:36 -0400

    beginstate

---
+++ BeginState.cpp
@@ -0,0 +1,17 @@
+#include "BeginState.h"
+
+#include "InClassProj.cpp"
+
+void BeginState::Update(float dt)
+{
+//std::vector<Sprite::Frame*> tile = ((InClassProj*)mStateMachine)->GetVec();
+
+	for (int i = 0; i < board.size(); ++i)
+	{
+		if (tile[i] != NULL)
+		{
+			tile[i]->
+		}
+	}
+	
+}
